88e2b502afe638bf7d5c6e651d8124f9f401a6ec
caetius <dlcasabuena@gmail.com>
2019-04-28 15:59:04 -0400

    Second Commit: Minor fixes.

--- denoise.py
+++ denoise.py
@@ -2,10 +2,6 @@ import numpy as np
 import torch
 
 def corrupt_input(corr_type, data, v):
-    """ Corrupt a fraction 'v' of 'data' according to the
-    noise method of this autoencoder.
-    :return: corrupted data
-    """
 
     if corr_type == 'mask':
         x_corrupted = masking_noise(data, v)
@@ -14,7 +10,7 @@ def corrupt_input(corr_type, data, v):
         x_corrupted = salt_and_pepper(data, v)
 
     elif corr_type == 'gauss':
-        x_corrupted = gaussian_noise(data, v, 0.2)
+        x_corrupted = gaussian_noise(data, v, 0.1)
 
     elif corr_type == 'none':
         x_corrupted = data
@@ -31,15 +27,15 @@ def masking_noise(X, v):
     return torch.mul(X, noise_tensor)
 
 '''Apply salt and pepper noise by setting a fraction v of the elements in X to the min and max values'''
-def salt_and_pepper(img, v):
-    rnd = np.random.rand(img.shape)
-    noisy = img[:]
+def salt_and_pepper(X, v):
+    rnd = torch.from_numpy(np.random.rand(X.shape[0], X.shape[1], X.shape[2], X.shape[3]))
+    noisy = X.clone()
     noisy[rnd < v/2] = 0.
-    noisy[rnd > 1 - v/2] = 1.
+    noisy[rnd > (1 - v/2)] = 1.
     return noisy
 
-'''TODO: Check if this works - Apply gaussian noise by adding values sampled from a gaussian to v of the elements in X to the min and max values'''
+'''Apply gaussian noise by adding values sampled from a gaussian to v of the elements in X to the min and max values'''
 def gaussian_noise(X, miu, std):
     noise = np.random.normal(loc=miu, scale=std, size=np.shape(X))
-    noise_t = torch.from_numpy(noise)
-    return X+noise_t # Normalize?
+    noise_t = torch.from_numpy(noise).type(torch.FloatTensor)
+    return torch.add(X,noise_t)
