0f4a68229e260539aa4483a97867faf12e36aab1
caetius <dlcasabuena@gmail.com>
2019-04-30 22:22:00 -0400

    Add files via upload

---
+++ Scripts/Prince/dae_pretrain_gauss.s
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:p40:1
+#SBATCH --time=48:00:00
+#SBATCH --mem=10GB
+#SBATCH --job-name=dae1
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=dlc423@nyu.edu
+#SBATCH --output=slurm_%j.out
+
+module load python3/intel/3.6.3 cudnn/9.0v7.0.5 cuda/9.0.176
+source ~/pyenv/py3.6.3/bin/activate
+
+export BASE_DIR=~/Shared/LeProject/DAE
+export NOISE_TYPE=gauss
+export NOISE_PERC=.05
+export VALID=False
+
+python $BASE_DIR/pretrain.py --perc_noise=$NOISE_PERC --corr_type=$NOISE_TYPE --valid=$VALID
